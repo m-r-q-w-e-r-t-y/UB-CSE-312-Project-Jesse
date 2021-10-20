42efdf086fab21ccc143aad2f6a563fd7e2a43ae
Ta7ar <imonkallprofessional@gmail.com>
2021-10-20 13:04:57 -0400

    Initial Signup page

--- server.py
+++ server.py
@@ -10,5 +10,9 @@ def index():
 def login():
     return render_template('login.html')
 
+@app.route("/signup",methods=['GET'])
+def signup():
+    return render_template('signup.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
