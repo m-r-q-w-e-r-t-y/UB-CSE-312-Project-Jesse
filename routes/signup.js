65b595bd1f446d6ce5308bc3d670cd99b5e2e78e
Ta7ar <imonkallprofessional@gmail.com>
2021-12-01 18:18:09 -0500

    Check if passwords match and save image only if checks pass

--- routes/signup.js
+++ routes/signup.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
+const path = require("path");
 const formidable = require("formidable");
-
 const signup = {};
 
 signup.GET = function (req, res) {
@@ -15,13 +15,7 @@ signup.GET = function (req, res) {
 };
 
 signup.POST = function (req, res) {
-  const form = formidable({
-    uploadDir: "uploads",
-    keepExtensions: true,
-    filter: function ({ name, originalFilename, mimetype }) {
-      return mimetype && mimetype.includes("image");
-    },
-  });
+  const form = formidable({ keepExtensions: true });
   form.parse(req, function (err, fields, files) {
     if (err) {
       res.writeHead(400, { "Content-Type": "text/plain" });
@@ -31,15 +25,33 @@ signup.POST = function (req, res) {
     const username = fields["username"];
     const password = fields["password"];
     const confirm_password = fields["confirm-password"];
-    const image_name =
-      files["avatar"] && files["avatar"].toJSON()["newFilename"];
+    const image = files["avatar"];
 
     //If any field is missing, return 400 error
-    if (!(username && password && confirm_password && image_name)) {
+    if (!(username && password && confirm_password && image)) {
       res.writeHead(400, { "Content-Type": "text/plain" });
       res.end("All fields must be filled");
       return;
     }
+
+    //If password fields do not match, return 400 error
+    if (password != confirm_password) {
+      res.writeHead(400, { "Content-Type": "text/plain" });
+      res.end("Passwords do not match");
+      return;
+    }
+
+    //Check filetype
+    if (!(image.mimetype && image.mimetype.includes("image"))) {
+      res.writeHead(400, { "Content-Type": "text/plain" });
+      res.end("Filetype must be image");
+      return;
+    }
+
+    //Save the image to server
+    const image_name = image && image.newFilename;
+    const image_data = fs.readFileSync(image.filepath);
+    fs.writeFileSync("uploads/" + image_name, image_data);
 
     res.writeHead(200, { "Content-Type": "application/json" });
     res.end(
