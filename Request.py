d929b081e8cdfc733ddb2943fd1e5773c8120f40
Ta7ar <imonkallprofessional@gmail.com>
2021-12-06 11:54:13 -0500

    Http Request parser

--- Request.py
+++ Request.py
@@ -1,50 +1,28 @@
-from collections import OrderedDict
-
 class Request:
-    startLine = None
-    headers = None
-    body = None
+    req_type: str = None
+    path: str = None
+    headers: dict = {}
+    body: bytes = None
 
-    def __init__(self, byteRequest):
-        req = byteRequest.split(b"\r\n\r\n")
-        headers = req[0].decode("utf-8").split("\r\n")
-        self.startLine = headers[0]
-
-        self.headers = OrderedDict()
-        for h in headers:
-            if ':' in h:
-                i = h.index(':')
-                k = h[:i]
-                v = h[i+2:]
-                self.headers[k] = v
-
-        if len(req) > 1:
-            self.body = req[1]
-    
-    def getStartLine(self):
-        return self.startLine
+    def __init__(self, data: bytes):
+        headers,body = data.split(b'\r\n\r\n',1)
+        headers = headers.decode("utf-8").split("\r\n")
+        startline = headers[0]
+        req_type,path,http_version = startline.split(" ")
+        for header in headers[1:]:
+            key,val = header.split(": ")
+            self.headers[key] = val
+        self.req_type = req_type
+        self.path = path
+        self.body = body
     
     def getHeaders(self):
-        return self.headers
-
-    def getBody(self):
-        return self.body
-
-    def getPath(self):
-        return self.startLine.split(" ")[1]
-
-    def getReqType(self):
-        return self.startLine.split(" ")[0]
+        return self.headers.keys()
 
     def toString(self):
-        string = self.startLine + "\n"
-
-        for key in self.headers:
-            string += key + ": " + self.headers[key] + "\n"
-        
-        string += "\n"
-
-        if self.body != None:
-            string += self.body
-
-        return string
+        return {
+            "Http Request Type": self.req_type,
+            "Path": self.path,
+            "Headers": self.headers,
+            "Body": self.body
+        }
