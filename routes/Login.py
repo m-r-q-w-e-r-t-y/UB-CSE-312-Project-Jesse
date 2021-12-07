d82a9819dde9d2f4343dd692814869e2d824f276
Ta7ar <imonkallprofessional@gmail.com>
2021-12-07 12:46:14 -0500

    Login POST route complete

--- routes/Login.py
+++ routes/Login.py
@@ -1,5 +1,8 @@
 from routes.Route import Route
 from Request import Request
+from db_init import User
+import bcrypt
+from routes.utility_functions import genAlphanumeric
 
 class Login(Route):
     def __init__(self, path, methods) -> None:
@@ -7,7 +10,21 @@ class Login(Route):
 
     def getResponse(self, request: Request):
         if request.req_type == "POST":
-            return Route.buildResponse()
+            form = request.form
+
+            if "username" not in form or "password" not in form:
+                return Route.buildResponse(400, {"Content-Type": "text/plain"},b'Fields cannot be empty')
+
+            username, password = form["username"].decode("utf-8"), form["password"]
+            user_record = User.getUserRecordByName(username)
+
+            if not user_record or not bcrypt.checkpw(password,user_record["password"].encode()):
+                return Route.buildResponse(401, {"Content-Type": "text/plain"},b'Username or password incorrect')
+
+            auth_token = genAlphanumeric()
+            User.updateAuthTokenByUsername(auth_token,username)
+            return Route.buildResponse(200,{"Content-Type":"text/plain", "Set-Cookie": f'authToken={auth_token};Max-Age=3600;HttpOnly'},b'Logged in with authToken '+auth_token.encode())
+
         elif request.req_type == "GET":
             try:
                 f = open("./public/login.html", "rb")
