7b901d33eb9e4b1ca049ef107fef3dc4956e166b
AntimatterAD <ansondai@buffalo.edu>
2021-12-07 00:34:00 -0500

    Added try and except

--- routes/Login.py
+++ routes/Login.py
@@ -9,8 +9,11 @@ class Login(Route):
         if request.req_type == "POST":
             return Route.buildResponse()
         elif request.req_type == "GET":
-            f = open("./public/login.html", "rb")
-            file = f.read()
-            file_length = str(len(file))
+            try:
+                f = open("./public/login.html", "rb")
+                file = f.read()
+                file_length = str(len(file))
 
-            return Route.buildResponse(200,{"Content-Type":"text/html","X-Content-Type-Options":"nosniff","Content-Length":file_length},file)
+                return Route.buildResponse(200, {"Content-Type": "text/html"}, file)
+            except Exception:
+                return Route.buildResponse(500, {"Content-Type": "text/plain"}, b'Internal Sever Error')
--- routes/Signup.py
+++ routes/Signup.py
@@ -23,8 +23,11 @@ class Signup(Route):
 
             return Route.buildResponse(200,{"Content-Type":"text/plain"},b'Account created')
         elif request.req_type == "GET":
-            f = open("./public/signup.html", "rb")
-            file = f.read()
-            file_length = str(len(file))
+            try:
+                f = open("./public/signup.html", "rb")
+                file = f.read()
+                file_length = str(len(file))
 
-            return Route.buildResponse(200,{"Content-Type":"text/html","X-Content-Type-Options":"nosniff","Content-Length":file_length},file)
+                return Route.buildResponse(200,{"Content-Type":"text/html"},file)
+            except Exception:
+                return Route.buildResponse(500, {"Content-Type":"text/plain"},b'Internal Sever Error')
