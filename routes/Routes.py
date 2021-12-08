227cf95c5f50390cffbefc843e50f9255883203a
Ta7ar <imonkallprofessional@gmail.com>
2021-12-08 15:01:15 -0500

    GET /lobby route working with redirects and html templating

--- routes/Routes.py
+++ routes/Routes.py
@@ -4,9 +4,11 @@ from routes.Signup import Signup as SignupRoute
 from routes.Login import Login as LoginRoute
 from routes.WebSocket import WebSocket as WebSocketRoute
 from routes.AuthTest import AuthTest as AuthTestRoute
+from routes.Lobby import Lobby as LobbyRoute
 
 
 routes: List[Route] = [
+    LobbyRoute("/",["GET"]),
     SignupRoute("/signup",["POST", "GET"]), 
     LoginRoute("/login",["POST","GET"]),
     WebSocketRoute("/websocket", ["GET"]),
