dfdf41c6ff740987c4aeb4d313e887ebdb748f37
Ta7ar <imonkallprofessional@gmail.com>
2021-12-10 03:35:29 -0500

    Send and handle socket data on frontend canvas

--- public/canvas.js
+++ public/canvas.js
@@ -6,60 +6,87 @@ const context = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-let actionSendCanvas = "SEND_CANVAS";
+const actionSendCanvas = "SEND_CANVAS";
 
-let mouseDown = false;
+let startX, startY;
+
 let color = randomColor();
 
 canvas.addEventListener("mousedown", startingCoordinates);
 canvas.addEventListener("mousemove", roamingCoordinates);
 
+/**
+ * Generates random RGB for each user connected to websocket
+ * @returns {String}
+ */
 function randomColor() {
   let r = Math.random() * 255;
   let g = Math.random() * 255;
   let b = Math.random() * 255;
+
+  r = Math.floor(r);
+  g = Math.floor(g);
+  b = Math.floor(b);
+
   return `rgb(${r}, ${g}, ${b})`;
 }
 
-socket.onmessage = function receivingData(message_event) {
-  const data = message_event.data;
-  roamingCoordinates(JSON.parse(data));
+/**
+ * On receiving socket messages, parses the data and draws on the canvas
+ * @param {MessageEvent} message_event
+ */
+socket.onmessage = function drawLines(message_event) {
+  const data = JSON.parse(message_event.data);
+
+  const x = data["x"],
+    y = data["y"],
+    endX = data["endX"],
+    endY = data["endY"],
+    color = data["color"];
+
+  context.beginPath();
+  context.lineWidth = 5;
+  context.moveTo(x, y);
+  context.lineTo(endX, endY);
+  context.strokeStyle = color;
+  context.stroke();
+  context.closePath();
 };
 
-function startingCoordinates(data) {
-  mouseDown = true;
+/**
+ * Sets the starting coordinates of the line
+ * @param {MouseEvent} event
+ */
+function startingCoordinates(event) {
   const { x, y } = canvas.getBoundingClientRect();
-  startX = data.clientX - x;
-  startY = data.clientY - y;
-  //console.log("x: " + startX + ", y: " + startY);
+  startX = event.clientX - x;
+  startY = event.clientY - y;
+}
+
+/**
+ * Sends coordinates of the drawn line to the websocket server
+ * @param {MouseEvent} event
+ */
+function roamingCoordinates(event) {
+  if (event.buttons !== 1) return;
+
+  const { x, y } = canvas.getBoundingClientRect();
+  const continuousX = event.clientX - x;
+  const continuousY = event.clientY - y;
+
   socket.send(
     JSON.stringify({
       x: startX,
       y: startY,
+      endX: continuousX,
+      endY: continuousY,
       color: color,
       webSocketAction: actionSendCanvas,
     })
   );
-}
-
-function roamingCoordinates(data) {
-  if (data.buttons !== 1) return;
-
-  const { x, y } = canvas.getBoundingClientRect();
-  const continuousX = data.clientX - x;
-  const continuousY = data.clientY - y;
-
-  context.beginPath();
-  context.lineWidth = 5;
-  context.moveTo(startX, startY);
-  context.lineTo(continuousX, continuousY);
-  context.strokeStyle = color;
-  context.stroke();
-  context.closePath();
 
   startX = continuousX;
   startY = continuousY;
-  //console.log("x: " + startX + ", y: " + startY);
 }
 
 function clearPage(e) {
